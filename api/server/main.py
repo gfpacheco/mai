40019afda4e49f6b0e65163d800106138189bf99
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2023-05-05 17:53:19 -0300

    Add api

---
+++ api/server/main.py
@@ -0,0 +1,133 @@
+from typing import Optional
+import uvicorn
+from fastapi import FastAPI, File, Form, HTTPException, Body, UploadFile
+import os
+
+from models.api import (
+    DeleteRequest,
+    DeleteResponse,
+    QueryRequest,
+    QueryResponse,
+    UpsertRequest,
+    UpsertResponse,
+)
+from datastore.factory import get_datastore
+from services.file import get_document_from_file
+
+from models.models import DocumentMetadata, Source
+from fastapi.middleware.cors import CORSMiddleware
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+ORIGINS = os.environ.get("ORIGINS", "*")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS.split(","),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post(
+    "/upsert-file",
+    response_model=UpsertResponse,
+)
+async def upsert_file(
+    file: UploadFile = File(...),
+    metadata: Optional[str] = Form(None),
+):
+    print("got here")
+    try:
+        metadata_obj = (
+            DocumentMetadata.parse_raw(metadata)
+            if metadata
+            else DocumentMetadata(source=Source.file)
+        )
+    except:
+        metadata_obj = DocumentMetadata(source=Source.file)
+
+    print("got here 2", metadata_obj)
+
+    document = await get_document_from_file(file, metadata_obj)
+
+    print("got here 3", document)
+
+    try:
+        ids = await datastore.upsert([document])
+        print("got here 4", ids)
+        return UpsertResponse(ids=ids)
+    except Exception as e:
+        print("Error:", e)
+        raise HTTPException(status_code=500, detail=f"str({e})")
+
+
+@app.post(
+    "/upsert",
+    response_model=UpsertResponse,
+)
+async def upsert(
+    request: UpsertRequest = Body(...),
+):
+    try:
+        ids = await datastore.upsert(request.documents)
+        return UpsertResponse(ids=ids)
+    except Exception as e:
+        print("Error:", e)
+        raise HTTPException(status_code=500, detail="Internal Service Error")
+
+
+@app.post(
+    "/query",
+    response_model=QueryResponse,
+)
+async def query_main(
+    request: QueryRequest = Body(...),
+):
+    try:
+        results = await datastore.query(
+            request.queries,
+        )
+        return QueryResponse(results=results)
+    except Exception as e:
+        print("Error:", e)
+        raise HTTPException(status_code=500, detail="Internal Service Error")
+
+
+@app.delete(
+    "/delete",
+    response_model=DeleteResponse,
+)
+async def delete(
+    request: DeleteRequest = Body(...),
+):
+    if not (request.ids or request.filter or request.delete_all):
+        raise HTTPException(
+            status_code=400,
+            detail="One of ids, filter, or delete_all is required",
+        )
+    try:
+        success = await datastore.delete(
+            ids=request.ids,
+            filter=request.filter,
+            delete_all=request.delete_all,
+        )
+        return DeleteResponse(success=success)
+    except Exception as e:
+        print("Error:", e)
+        raise HTTPException(status_code=500, detail="Internal Service Error")
+
+
+@app.on_event("startup")
+async def startup():
+    global datastore
+    datastore = await get_datastore()
+
+
+def start():
+    uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
