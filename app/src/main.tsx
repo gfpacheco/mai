c032b1ef5d29527990ce272fb0958946340d7604
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2023-05-08 08:43:33 -0300

    Add app components folder

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 
-import { App } from './App.tsx';
+import { App } from './components/App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
