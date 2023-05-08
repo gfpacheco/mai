7f4b360f029b3453dc0673c0e5b370d9f3fb0bf3
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2023-05-08 15:02:26 -0300

    Clean up api

--- api/datastore/factory.py
+++ api/datastore/factory.py
@@ -7,33 +7,9 @@ async def get_datastore() -> DataStore:
     assert datastore is not None
 
     match datastore:
-        case "llama":
-            from api.datastore.providers.llama_datastore import LlamaDataStore
-            return LlamaDataStore()
-
-        case "pinecone":
-            from api.datastore.providers.pinecone_datastore import PineconeDataStore
-
-            return PineconeDataStore()
         case "weaviate":
             from api.datastore.providers.weaviate_datastore import WeaviateDataStore
 
             return WeaviateDataStore()
-        case "milvus":
-            from api.datastore.providers.milvus_datastore import MilvusDataStore
-
-            return MilvusDataStore()
-        case "zilliz":
-            from api.datastore.providers.zilliz_datastore import ZillizDataStore
-
-            return ZillizDataStore()
-        case "redis":
-            from api.datastore.providers.redis_datastore import RedisDataStore
-
-            return await RedisDataStore.init()
-        case "qdrant":
-            from api.datastore.providers.qdrant_datastore import QdrantDataStore
-
-            return QdrantDataStore()
         case _:
             raise ValueError(f"Unsupported vector database: {datastore}")
